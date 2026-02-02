9b47b2de577d426bf0d5c1ed440860a47679382f
Yamao, Sosuke/山尾 創輔 <yamao.sosuke@fujitsu.com>
2026-02-02 13:33:47 +0900

    Add default values and documentation to shell scripts
    - Set default FILE_PATH to kg_factory dataset in run_import.sh
    - Set default QUERY in run_kg_rag.sh
    - Improve run_episode_retriever.sh with conditional arguments
    - Add comments explaining available datasets and example queries

--- script/run_import.sh
+++ script/run_import.sh
@@ -3,7 +3,11 @@
 # This script calls the import_db.py script with a predefined file path.
 
 # Define the file path to import
-FILE_PATH=""
+# Use relative path from Neo4j import directory (mounted from ./neo4j_import)
+# Example (kg_factory):
+#   FILE_PATH="FieldWork_Knowledge_Dataset/kg_factory/kg_factory_incident_count.graphml"
+# See FieldWork_Knowledge_Dataset/ for all available .graphml files
+FILE_PATH="FieldWork_Knowledge_Dataset/kg_factory/kg_factory_incident_count.graphml"
 
 # Run the import_db.py script with the file path
 python3 import_db.py --file_path "$FILE_PATH"
